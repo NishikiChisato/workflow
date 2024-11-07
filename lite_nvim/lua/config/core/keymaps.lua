c3371a6b50030655b54252c30136916e1d6289fc
NishikiChisato <NishikiChisato@outlook.com>
2024-11-07 09:09:45 +0800

    [feat] we used auto-script to update lite neovim configuration

--- lite_nvim/lua/config/core/keymaps.lua
+++ lite_nvim/lua/config/core/keymaps.lua
@@ -11,6 +11,7 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
+-- splits
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make split equal size" })
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split winfow horizontally" })
@@ -26,15 +27,59 @@ keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Go right" })
 keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Go down" })
 keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Go up" })
 
+-- tabs
 keymap.set("n", "<leader>to", ":tabnew<CR>", { desc = "Open new tab" })
 keymap.set("n", "<leader>tx", ":tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tb", ":tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+-- tab movement
 keymap.set("n", "<leader>tn", "gt", { desc = "Goto next tab" })
 keymap.set("n", "<leader>tp", "gT", { desc = "Goto previous tab" })
-keymap.set("n", "<leader>tb", ":tabnew %<CR>", { desc = "Open current buffer in new tab" })
+keymap.set("n", "<A-l>", "gt", { desc = "Goto next tab" })
+keymap.set("n", "<A-h>", "gT", { desc = "Goto previous tab" })
+
+-- move tabs
+keymap.set("n", "<A-S-h>", ":tabm -1<CR>", { desc = "Move tab to left" })
+keymap.set("n", "<A-S-l>", ":tabm +1<CR>", { desc = "Move tab to right" })
+keymap.set("n", "<A-S-0>", ":tabm 0<CR>", { desc = "Move tab to first" })
+keymap.set("n", "<A-S-$>", ":tabm $<CR>", { desc = "Move tab to last" })
+
+-- terminal
+function OpenVerticalTabWithTerminal()
+    vim.cmd("vsplit")
+    vim.cmd("terminal")
+end
+keymap.set("n", "<leader>tc", ":lua OpenVerticalTabWithTerminal()<CR>", { desc = "Open terminal in current tab" })
+keymap.set("t", "<ESC>", "<C-\\><C-n>", { desc = "Exit terminal" })
+keymap.set("t", "<C-j><C-k>", "<C-\\><C-n>", { desc = "Exit terminal" })
 
 keymap.set("n", "<leader>jd", "<C-]>", { desc = "Jump to tags" })
 keymap.set("n", "]]", "<C-I>", { desc = "Jump to next tags" })
 keymap.set("n", "[[", "<C-O>", { desc = "Jump to prev tags" })
+
+-- tags
+keymap.set("n", "<leader>jd", "<C-]>", { desc = "Jump to tags" })
+keymap.set("n", "]]", "<C-I>", { desc = "Jump to next tags" })
+keymap.set("n", "[[", "<C-O>", { desc = "Jump to prev tags" })
+
+-- insert mode
+keymap.set("i", "<C-u>", "<C-o>u", { desc = "Undo" })
+keymap.set("i", "<C-r>", "<C-o><C-r>", { desc = "Redo" })
+
+keymap.set("i", "<C-h>", "<Left>", { desc = "Move left" })
+keymap.set("i", "<C-j>", "<Down>", { desc = "Move down" })
+keymap.set("i", "<C-k>", "<Up>", { desc = "Move up" })
+keymap.set("i", "<C-l>", "<Right>", { desc = "Move right" })
+
+-- some scenario optimized
+-- Pasting a deleted or copied line directly after pressing o to create a new line in Neovim
+keymap.set("i", "<C-p>", "<ESC>p'[i", { desc = "Quick paste in insert mode" })
+--[=[
+--  <ESC> -> back to normal mode
+--  p -> paste the last yanked or deleted line
+--  '[ -> move the cursor to the beginning of pasted line
+--  i -> beck to insert mode
+--]=]
 
 -- plugins keymaps
 
