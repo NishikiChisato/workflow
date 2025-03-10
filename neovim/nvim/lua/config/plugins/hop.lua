c51eb4829359c6dcfba03cf16c8865db9510293f
NishikiChisato <NishikiChisato@outlook.com>
2025-03-10 08:08:27 +0800

    bugfix: resolve kapmaps conflicts

--- neovim/nvim/lua/config/plugins/hop.lua
+++ neovim/nvim/lua/config/plugins/hop.lua
@@ -7,11 +7,11 @@ return {
     hop.setup(opts)
     local directions = require("hop.hint").HintDirection
     local keymap = vim.keymap
-    keymap.set("n", "<leader>hC", function() hop.hint_char1({direction = directions.BEFORE_CURSOR}) end, { desc = "Jump to chars(before cursor)" })
-    keymap.set("n", "<leader>hc", function() hop.hint_char1({direction = directions.AFTER_CURSOR}) end, { desc = "Jump to chars(after cursor)" })
-    keymap.set("n", "<leader>hW", function() hop.hint_words({direction = directions.BEFORE_CURSOR}) end, { desc = "Jump to words(before cursor)" })
-    keymap.set("n", "<leader>hw", function() hop.hint_words({direction = directions.AFTER_CURSOR}) end, { desc = "Jump to words(after cursor)" })
-    keymap.set("n", "<leader>hP", function() hop.hint_patterns({direction = directions.BEFORE_CURSOR}) end, { desc = "Jump to pattern(before cursor)" })
-    keymap.set("n", "<leader>hp", function() hop.hint_patterns({direction = directions.AFTER_CURSOR}) end, { desc = "Jump to pattern(after cursor)" })
+    keymap.set("n", "<leader>jC", function() hop.hint_char1({direction = directions.BEFORE_CURSOR}) end, { desc = "Jump to chars(before cursor)" })
+    keymap.set("n", "<leader>jc", function() hop.hint_char1({direction = directions.AFTER_CURSOR}) end, { desc = "Jump to chars(after cursor)" })
+    keymap.set("n", "<leader>jW", function() hop.hint_words({direction = directions.BEFORE_CURSOR}) end, { desc = "Jump to words(before cursor)" })
+    keymap.set("n", "<leader>jw", function() hop.hint_words({direction = directions.AFTER_CURSOR}) end, { desc = "Jump to words(after cursor)" })
+    keymap.set("n", "<leader>jP", function() hop.hint_patterns({direction = directions.BEFORE_CURSOR}) end, { desc = "Jump to pattern(before cursor)" })
+    keymap.set("n", "<leader>jp", function() hop.hint_patterns({direction = directions.AFTER_CURSOR}) end, { desc = "Jump to pattern(after cursor)" })
   end,
 }
